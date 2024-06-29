63a03394d084af013e19d01163a9ec08ebd85038
Chriver68 <118003889+Chriver68@users.noreply.github.com>
2024-06-29 02:19:57 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import streamlit as st
+import re
+from main import *
+
+st.set_page_config(page_title='F1 2024 Dashboard',
+                   page_icon='bar_chart',
+                   layout='wide')
+
+col1, col2, col3= st.columns([2,3,1])
+col2.title('F1 Points season 2024.')
+
+colA, colB, colC = st.columns([1,1,1])
+colB.write(f'Last updated {most_recent_date.date()}.  Grandprix: {most_recent_race}')
+
+col4, col5, col6= st.columns([3,1,3])
+
+with col4:
+    st.header("Total points Graphic")
+    st.write(fig)
+
+with col5:
+    st.header("Total points")
+    st.dataframe(punten_cumulatief, hide_index=True, height=800)
+
+with col6:
+    fig2 = px.line(df, x='Grandprix', y='Grouped Cumulative Sum', color='Driver', text='Points')
+    fig2 = fig2.update_layout(
+        autosize=False,
+        width=800,
+        height=800,)
+    st.header(f"Build up Total points")
+    st.write(fig2)
+
